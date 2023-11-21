789769de6c953c87e098e2b14e3a69101d7844d3
ema_r <ee207008@meiji.ac.jp>
2023-11-21 11:35:36 +0900

    ユーザーが操作する機能の仮実装

--- src/match_bo_game.py
+++ src/match_bo_game.py
@@ -162,6 +162,34 @@ def attack_random(attack_player_num, receive_player_num):
     else:
       match_value[receive_player_num][1] += match_value[attack_player_num][attack_hand_num]
       
+def attack_user(attack_player_num, receive_player_num):
+    attack_hand_num = 0
+    receive_hand_num = 0
+    
+    # 自分のどちらの手で攻撃するかの決定
+    if(match_value[attack_player_num][0] >= 5):
+      attack_hand_num = 1
+    elif(match_value[attack_player_num][1] >= 5):
+      attack_hand_num = 0
+    
+    elif(random() < 0.5):
+      attack_hand_num= 0
+    else :
+      attack_hand_num = 1
+
+    # 相手のどちらかの手を攻撃するかの決定と攻撃
+    if (match_value[receive_player_num][0] >= 5):
+      receive_hand_num = 1
+    elif (match_value[receive_player_num][1] >= 5):
+      receive_hand_num = 0
+      
+    elif (random() < 0.5) :
+      receive_hand_num = 0
+    else:
+      receive_hand_num = 1
+
+    match_value[receive_player_num][receive_hand_num] += match_value[attack_player_num][attack_hand_num]
+    
 def attack_mod_random(attack_player_num, receive_player_num):
     attack_hand_num = 0
     receive_hand_num = 0
